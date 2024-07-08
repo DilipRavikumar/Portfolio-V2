fa92af46df8c4ddf798f7493c380a7359c8a524b
Dilip R <mailthistodilip@gmail.com>
2024-07-09 02:51:51 +0530

    .

--- Formatted code/formatted.js
+++ Formatted code/formatted.js
@@ -120,7 +120,7 @@ function scrolltoTopfunction(){
 // document.addEventListener("contextmenu", function (e){
 //   e.preventDefault();
 // }, false);
-// https://github.com/Dilip.Rjangid07
+// https://github.com/Dilip.R07
 document.addEventListener("contextmenu", function(e){
   if (e.target.nodeName === "IMG") {
       e.preventDefault();
